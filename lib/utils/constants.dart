f917780b87e4c6be950d4929e3f488f6f6391c48
Pawan Avhad <154080782+pawanavhad@users.noreply.github.com>
2026-02-26 21:54:39 +0530

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String apiKey = "18fc0d97b44b355ee520b43fa78f71bd";
+  static const String apiKey = "YOUR_API_KEY";
 
   static const String baseUrl =
       "https://api.openweathermap.org/data/2.5/weather";
